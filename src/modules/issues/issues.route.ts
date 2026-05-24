c61924081cab666567baef9864c30a14524b95bf
Maptaul <maptaulislam1@gmail.com>
2026-05-24 16:21:43 +0600

    refactor: format issue routes for improved readability and consistency

--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -5,10 +5,22 @@ import { USER_ROLE } from "../../types";
 
 const router = Router();
 
-router.post("/", auth([USER_ROLE.contributor, USER_ROLE.maintainer]), issueController.createIssue);
-router.get("/", issueController.getAllIssues);
+router.post(
+  "/",
+  auth([USER_ROLE.contributor, USER_ROLE.maintainer]),
+  issueController.createIssue,
+);
+router.get("/", auth([USER_ROLE.maintainer]), issueController.getAllIssues);
 router.get("/:id", issueController.getIssueById);
-router.patch("/:id", auth([USER_ROLE.contributor, USER_ROLE.maintainer]), issueController.updateIssue);
-router.delete("/:id", auth([USER_ROLE.maintainer]), issueController.deleteIssue);
+router.patch(
+  "/:id",
+  auth([USER_ROLE.contributor, USER_ROLE.maintainer]),
+  issueController.updateIssue,
+);
+router.delete(
+  "/:id",
+  auth([USER_ROLE.maintainer]),
+  issueController.deleteIssue,
+);
 
 export const issueRoute = router;
